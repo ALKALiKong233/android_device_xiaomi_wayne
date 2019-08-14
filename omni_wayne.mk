98a9f6e368deb3a15f395efd748b09e5f6cfd1dd
Max Weffers <rcstar6696@gmail.com>
2019-08-14 09:22:03 +0200

    wayne: Omni-fy Mi 6X

--- omni_wayne.mk
+++ omni_wayne.mk
@@ -24,18 +24,25 @@
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/wayne/device.mk)
 
-# Inherit from Havoc custom product configuration
-$(call inherit-product, vendor/havoc/config/common.mk)
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Build Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRIVATE_BUILD_DESC="wayne-user 9 PKQ1.180904.001 V10.3.4.0.PDCCNXM release-keys"
+
+BUILD_FINGERPRINT="xiaomi/wayne/wayne:9/PKQ1.180904.001/V10.3.4.0.PDCCNXM:user/release-keys"
+
 
 TARGET_VENDOR_PRODUCT_NAME := wayne
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.havoc.maintainer=rcstar6696
 
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PLATFORM := SDM660
-PRODUCT_NAME := havoc_wayne
+PRODUCT_NAME := omni_wayne
 PRODUCT_DEVICE := wayne
 PRODUCT_MODEL := MI 6X
